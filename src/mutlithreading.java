35433ed64ec1f9c1f0dd49f4f22cc0b91642b715
Yash Lad <71868797+yashlad27@users.noreply.github.com>
2022-04-19 12:15:54 +0530

    commit 04

--- src/mutlithreading.java
+++ src/mutlithreading.java
@@ -28,14 +28,3 @@ class multithreaddemo extends Thread{
     }
 }
 
-class multithreadingDemo implements Runnable{
-    @Override
-    public void run() {
-        try{
-            System.out.println("Thread "+Thread.currentThread()+" is running.");
-        }
-        catch(Exception e){
-            System.out.println("Exception is caught!");
-        }
-    }
-}
